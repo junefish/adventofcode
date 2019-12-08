ea5ec6f56ad598443e61fd201c3fafed63c1f45b
junefish <junefish@users.noreply.github.com>
2019-12-07 20:17:42 -0500

    day 1 part 1

---
+++ Documents/adventofcode/day1/day1_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+def calcFuel(mass):
+   fuel  = round(mass/3)-2
+   return int(fuel)
+
+sum = 0
+with open('day1input.txt', 'r') as input:
+    for line in input:
+        sum += calcFuel(int(line))
+
+print("Test values:")
+print(calcFuel(12))
+print(calcFuel(14))
+print(calcFuel(1969))
+print(calcFuel(100756))
+
+print("Puzzle answer: " + str(sum))
