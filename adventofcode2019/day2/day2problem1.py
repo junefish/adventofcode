6ebf1007c3eec107727d2cd51f55f4b9e81a3001
junefish <junefish@users.noreply.github.com>
2019-12-08 12:52:49 -0500

    add day 2 problem 1 solution

--- adventofcode2019/day2/day2problem1.py
+++ adventofcode2019/day2/day2problem1.py
@@ -2,7 +2,26 @@
 
 with open('day2input.txt', 'r') as file:
     input = file.read()
-    intcode = input.split(',')
+    intcode = map(int,input.split(','))
 
-for i in range(0,3):
-    print(intcode[i])
+def alarm(intcode):
+    intcode[1] = 12
+    intcode[2] = 2
+
+def opcode(intcode):
+    for i in range(0,len(intcode)-1,4):
+        pos1 = intcode[i]
+        pos2 = intcode[i+1]
+        pos3 = intcode[i+2]
+        pos4 = intcode[i+3]
+
+        if(pos1 == 1):
+            intcode[pos4] = intcode[pos2] + intcode[pos3]
+        elif(pos1 == 2):
+            intcode[pos4] = intcode[pos2] * intcode[pos3]
+        elif(pos1 == 99):
+            break
+    return intcode
+
+alarm(intcode)
+print("The full altered intcode is:\n" + str(opcode(intcode)))
