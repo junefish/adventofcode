906527543076bd98ce86e522430716a4ae8e39cb
junefish <junefish@users.noreply.github.com>
2019-12-07 22:29:08 -0500

    reorganise directory structure

--- adventofcode/day2/day2problem1.py
+++ adventofcode/day2/day2problem1.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 
-with open('day2input.txt', 'r') as input:
+with open('day2input.txt', 'r') as file:
+    input = file.read()
     intcode = input.split(',')
 
-for i in range[0,3]:
-    print(intcode(i))
+for i in range(0,3):
+    print(intcode[i])
