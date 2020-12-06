abbd9f326602c8bb67e304a5aea05c4cf99a42fa
junefish <junefish@users.noreply.github.com>
2020-12-06 15:27:42 -0500

    Update day4problem1.py
    fix indentation level

--- adventofcode2020/day4/day4problem1.py
+++ adventofcode2020/day4/day4problem1.py
@@ -1,9 +1,9 @@
 passports = []
 
 with open('adventofcode2020\day4\day4input.txt', 'r') as input:
-        passport_file = input.read().split('\n\n')
-        for line in passport_file:
-            passports.append(line.replace('\n', ' '))
+    passport_file = input.read().split('\n\n')
+    for line in passport_file:
+        passports.append(line.replace('\n', ' '))
 
 valid_passports = 0
 keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
