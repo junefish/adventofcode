4acebdf3fb1343fc6f3e7749b167260bd4a10728
junefish <junefish@users.noreply.github.com>
2020-12-05 22:49:42 -0500

    Update day2problem1.py
    add solution

--- adventofcode2020/day2/day2problem1.py
+++ adventofcode2020/day2/day2problem1.py
@@ -1,8 +1,12 @@
-entries = []
+valid_passwords = 0
 
 with open('adventofcode2020\day2\day2input.txt', 'r') as input:
   for line in input:
-    item = line.split('\n')
-    entries.append(item[0])
+    policy, password = line.split(':')
+    
+    char = policy[-1]
+    min, max = map(int, policy[:-2].split('-'))
 
-print(entries)
+    if password.count(char) in range(min, max + 1):
+       valid_passwords += 1
+  print(valid_passwords)
