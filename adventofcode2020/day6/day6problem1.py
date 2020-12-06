addefeb9de9c7e6044d460edb50a2e00102b408b
junefish <junefish@users.noreply.github.com>
2020-12-06 15:43:49 -0500

    fix deduping of groups with the same answers

--- adventofcode2020/day6/day6problem1.py
+++ adventofcode2020/day6/day6problem1.py
@@ -3,19 +3,18 @@ answers = []
 # open file
 with open('adventofcode2020\day6\day6input.txt', 'r') as input:
     customs_file = input.read().split('\n\n')
-    for line in customs_file:
-        answers.append(line.replace('\n', ''))
+
+for line in customs_file:
+    answers.append(line.replace('\n', ''))
             
 # remove duplicate answers within a group
 uniques = []
 for answer in answers:
     uniques.append("".join(set(answer)))
 
-groups = dict.fromkeys(uniques)
+groups = []
 
-# count number of answers per group
-for key, value in groups.items():
-    groups[key] = len(key)
+for item in uniques:
+    groups.append(len(item))
 
-# calculate sum of answers from all groups
-print(sum(groups.values()))
+print(sum(groups))
