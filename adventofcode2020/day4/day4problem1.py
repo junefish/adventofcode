e4f26633e107447c0695f7c0b568f46f4928e2de
junefish <junefish@users.noreply.github.com>
2020-12-06 12:24:43 -0500

    Create day4problem1.py
    add solution

---
+++ adventofcode2020/day4/day4problem1.py
@@ -0,0 +1,15 @@
+passports = []
+
+with open('adventofcode2020\day4\day4input.txt', 'r') as input:
+        passport_file = input.read().split('\n\n')
+        for line in passport_file:
+            passports.append(line.replace('\n', ' '))
+
+valid_passports = 0
+keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+for passport in passports:
+    if all(key + ':' in passport for key in keys):
+        valid_passports += 1
+
+print(valid_passports)
