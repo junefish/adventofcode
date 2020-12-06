212e8793350a1f5c03aeeb3ae5a888510669b7b1
junefish <junefish@users.noreply.github.com>
2020-12-06 14:12:06 -0500

    Create day5problem1.py
    add solution

---
+++ adventofcode2020/day5/day5problem1.py
@@ -0,0 +1,18 @@
+with open('adventofcode2020\day5\day5input.txt', 'r') as input:
+    seats = [line.rstrip() for line in input]
+
+rows = []
+cols = []
+seat_ids = []
+char_to_replace = {'F' : '0', 'B' : '1', 'L' : '0', 'R' : '1'}
+for seat in seats:
+    row = slice(7)
+    rows.append(seat[row].translate(str.maketrans(char_to_replace)))
+
+    col = seat[-3:]
+    cols.append(col.translate(str.maketrans(char_to_replace)))
+
+for i in range(len(seats)):
+    seat_ids.append(int(rows[i], 2) * 8 + int(cols[i], 2))
+
+print(max(seat_ids))
