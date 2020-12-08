9a920ecb998a68d2a247d762ba7d5799e86465a3
junefish <junefish@users.noreply.github.com>
2020-12-08 00:13:38 -0500

    Update day6problem2.py

--- adventofcode2020/day6/day6problem2.py
+++ adventofcode2020/day6/day6problem2.py
@@ -1,24 +1,14 @@
 answers = []
+count = 0
 
-
-# open file
 with open('adventofcode2020\day6\day6input.txt', 'r') as input:
-    customs_file = input.read().split('\n\n')
+    customs_file = input.read().splitlines()
 
 for line in customs_file:
-    answers.append(line.split('\n'))
-
-# print(answers)
-
-for answer in answers:
-    count = 0
-    keys = list(answer[0])
-    if(len(answers) == 1):
-        count = answer[0]
+    if(line == ''):
+        count += len(set.intersection(*answers))
+        answers = []
     else:
-        break
-        # i = 1
-        # while(i < len(answers)):
-        #     if all(key in answer[i] for key in keys):
-        #         count += 1
+        answers.append(set(line))
+
 print(count)
