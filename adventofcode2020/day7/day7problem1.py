f20746921a5c54ab2565c12b9709d0042e02f499
junefish <junefish@users.noreply.github.com>
2020-12-08 23:28:53 -0500

    Update day7problem1.py

--- adventofcode2020/day7/day7problem1.py
+++ adventofcode2020/day7/day7problem1.py
@@ -2,10 +2,15 @@ with open('adventofcode2020\day7\day7input.txt', 'r') as input:
     rules = [line.rstrip() for line in input]
 
 bag_colors = {}
+parent_bags = []
 for rule in rules:
     words = rule.split(' ')
     color = words[0] + ' ' + words[1]
     contents = ' '.join([str(word) for word in words[4:]]) 
     bag_colors.update({color : contents})
 
-print(bag_colors)
+    if('shiny gold' in contents):
+        parent_bags.append(color)
+        
+# for parent in parent_bags:
+#     if(parent in contents)
