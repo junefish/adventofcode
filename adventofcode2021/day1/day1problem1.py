56dcf70452a215bc9ff135542ecaa2d23c84aec1
junefish <junefish@users.noreply.github.com>
2021-12-05 18:57:33 -0500

    Create day1problem1.py

---
+++ adventofcode2021/day1/day1problem1.py
@@ -0,0 +1,18 @@
+depths = []
+
+with open('adventofcode2021\day1\day1input.txt', 'r') as input:
+  for line in input:
+    item = line.split('\n')
+    depths.append(int(item[0]))
+
+deltas = []
+for i in range(0, len(depths)):
+    if(i == 0):
+        delta = "N/A - no previous measurement"
+    elif(depths[i] > depths[i-1]):
+        delta = "increased"
+    elif(depths[i] < depths[i-1]):
+        delta = "decreased"
+    deltas.append(delta)
+
+print(deltas.count("increased"))
