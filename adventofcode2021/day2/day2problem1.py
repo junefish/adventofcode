00a1a5ba8a95cb23260e61ed4f13185b9ddb1736
junefish <junefish@users.noreply.github.com>
2021-12-05 19:53:31 -0500

    delete test print statements

--- adventofcode2021/day2/day2problem1.py
+++ adventofcode2021/day2/day2problem1.py
@@ -4,17 +4,14 @@ with open('adventofcode2021\day2\day2input.txt', 'r') as input:
     item = line.split('\n')
     command = (item[0].split(' '))
     commands.append((str(command[0]), int(command[1])))
-#print(commands)
 
 horizontal = 0
 depth = 0
 for command in commands:
-    #print(command)
     if(command[0] == 'forward'):
         horizontal += command[1]
     elif(command[0] == 'down'):
         depth += command[1]
     elif(command[0] == 'up'):
         depth -= command[1]
-    #print(horizontal,depth)
 print(horizontal * depth)
