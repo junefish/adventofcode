a834122cd41839501e516c1e7d32110b069279ff
junefish <junefish@users.noreply.github.com>
2021-12-05 22:48:23 -0500

    Update day3problem2.py

--- adventofcode2021/day3/day3problem2.py
+++ adventofcode2021/day3/day3problem2.py
@@ -7,8 +7,20 @@ with open('adventofcode2021\day3\day3example.txt', 'r') as input:
     diagnostics.append(str(item[0]))
 
 digits = []
-for i in range(0, len(diagnostics)):
+for i in range(len(diagnostics)):
     digits.append(list(diagnostics[i]))
 
-oxygen_test = digits
-print(oxygen_test)
+number_length = len(digits[0])
+digit_lists = [[] for x in range(number_length)]
+
+for entry in digits:
+    for i in range(number_length):
+        digit_lists[i].append(entry[i])
+
+oxygen_test = diagnostics
+for i in range(len(digit_lists)):
+    digit = digit_lists[i]
+    bit = ['1' for x in range(number_length)]
+    if(digit.count('0') != digit.count('1')):
+        bit[i] = str(mode(digit))
+print(bit)
