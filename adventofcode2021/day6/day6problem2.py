9baa74a8121ea9d241e24f50480df51c591490aa
junefish <junefish@users.noreply.github.com>
2021-12-30 15:46:19 -0500

    add solution

--- adventofcode2021/day6/day6problem2.py
+++ adventofcode2021/day6/day6problem2.py
@@ -1,21 +1,31 @@
+from typing import DefaultDict
+
+
 fish_timers = []
-with open('adventofcode2021\day6\day6example.txt', 'r') as input:
+with open('adventofcode2021\day6\day6input.txt', 'r') as input:
   for line in input:
     item = line.split('\n')
     fish_timers = item[0].split(',')
 
 fish_timers = list(map(int, fish_timers))
+fish_map = {}
+for fish in fish_timers:
+    if fish not in fish_map:
+        fish_map[fish] = 0
+    fish_map[fish] += 1
 
 day = 1
 while(day <= 256):
-    for i in range(0, len(fish_timers)):
-        timer = fish_timers[i]
-        if(timer == 0):
-            fish = 6
-            fish_timers.append(8)
+    updated_map = DefaultDict(int)
+
+    for fish, count in fish_map.items():
+        if fish == 0:
+            updated_map[6] += count
+            updated_map[8] += count
         else:
-            fish = timer - 1
-        fish_timers[i] = fish
+            updated_map[fish - 1] += count
+
+        fish_map = updated_map
 
     day += 1
-print(len(fish_timers))
+print(sum(fish_map.values()))
