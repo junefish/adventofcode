3122740801ded78671577e3938d0f45ee024e494
junefish <junefish@users.noreply.github.com>
2021-12-30 23:21:16 -0500

    remove print statements

--- adventofcode2021/day5/day5problem1.py
+++ adventofcode2021/day5/day5problem1.py
@@ -4,15 +4,11 @@ from typing import Counter
 with open('adventofcode2021\day5\day5input.txt', 'r') as input:
   lines = input.read().splitlines()
 
-# print(lines)
-
 all_points = []
 for line in lines:
     point1, point2 = line.split('->')
     x1, y1 = tuple(map(int, point1.split(',')))
     x2, y2 = tuple(map(int, point2.split(',')))
-
-    # print(x1, y1, x2, y2)
 
     if(x1 == x2 or y1 == y2):
         for x in range(min(x1, x2), max(x1, x2) + 1):
