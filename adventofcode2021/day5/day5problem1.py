ec3b0ba15be3512bda07607c42b4553f9c1b0e17
junefish <junefish@users.noreply.github.com>
2021-12-30 23:27:23 -0500

    add solution

--- adventofcode2021/day5/day5problem1.py
+++ adventofcode2021/day5/day5problem1.py
@@ -15,4 +15,13 @@ for line in lines:
             for y in range (min(y1, y2), max(y1, y2) + 1):
                 all_points.append((x,y))
 
+    else:
+        x_increment = 1 if x1 < x2 else -1
+        y_increment = 1 if y1 < y2 else -1
+
+        y = y1
+        for x in range(x1, x2 + x_increment, x_increment):
+            all_points.append((x,y))
+            y += y_increment
+
 print(len([point for point in Counter(all_points).values() if point > 1]))
