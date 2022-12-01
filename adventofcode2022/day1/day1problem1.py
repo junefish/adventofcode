4052bf87bd255feed937ed887fa9009251e148e3
junefish <junefish@users.noreply.github.com>
2022-12-01 12:24:53 -0500

    assign items to elves

--- adventofcode2022/day1/day1problem1.py
+++ adventofcode2022/day1/day1problem1.py
@@ -10,3 +10,12 @@ with open('adventofcode2022/day1/day1input.txt', 'r') as input:
 elves = [[]]*(items.count('')+1)
 
 # print(len(elves))
+
+i = 0
+delimiter = ''
+for item in items:
+    print("elf" + str(i) + ": " + item)
+    if item != delimiter: 
+        elves[i].append(item)
+    elif item == delimiter:
+        i = i + 1
