15bdf72ff660eed8baa5538995528a8a59c5ec22
junefish <junefish@users.noreply.github.com>
2022-12-01 21:22:36 -0500

    Create day1problem2.py

---
+++ adventofcode2022/day1/day1problem2.py
@@ -0,0 +1,21 @@
+# initialise lists
+items = []
+elves = [[]]
+calories = []
+
+# read input file
+with open('adventofcode2022/day1/day1input.txt', 'r') as input:
+    for line in input:
+        items.append(line.strip())
+        
+# assign items to elves
+delimiter = ''
+for item in items:
+    if item == delimiter:
+        elves.append([])
+    elif item != delimiter: 
+        elves[-1].append(int(item))
+
+# calculate calories carried per elf
+for elf in elves:
+    calories.append(sum(elf))
