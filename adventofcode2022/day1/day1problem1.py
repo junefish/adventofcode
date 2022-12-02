a6c1fb7c7a95597ec105bfacbddcb5ca8b34400a
junefish <junefish@users.noreply.github.com>
2022-12-01 21:10:38 -0500

    fix elf item parsing

--- adventofcode2022/day1/day1problem1.py
+++ adventofcode2022/day1/day1problem1.py
@@ -1,21 +1,19 @@
 items = []
 
-with open('adventofcode2022/day1/day1input.txt', 'r') as input:
+with open('adventofcode2022/day1/day1example.txt', 'r') as input:
     for line in input:
         items.append(line.strip())
 
 # print(items)
 # print(items[0])
 
-elves = [[]]*(items.count('')+1)
+elves = [[]]
 
-# print(len(elves))
-
-i = 0
 delimiter = ''
 for item in items:
-    print("elf" + str(i) + ": " + item)
-    if item != delimiter: 
-        elves[i].append(item)
-    elif item == delimiter:
-        i = i + 1
+    if item == delimiter:
+        elves.append([])
+    elif item != delimiter: 
+        elves[-1].append(item)
+        
+# print(elves)
