b4f6a2d9af9da5f6786cd4b0c3e804932a00b8c8
junefish <junefish@users.noreply.github.com>
2022-12-02 13:28:55 -0500

    switch to problem input

--- adventofcode2022/day2/day2problem1.py
+++ adventofcode2022/day2/day2problem1.py
@@ -7,7 +7,7 @@ outcomes = {'win': 6, 'draw': 3, 'loss': 0}
 points = {'X': 1, 'Y': 2, 'Z': 3}
 
 # read input file
-with open('adventofcode2022/day2/day2example.txt', 'r') as input:
+with open('adventofcode2022/day2/day2input.txt', 'r') as input:
     for line in input:
         round = (line.strip())
         rounds.append(round.split(' '))
