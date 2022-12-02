eb9181e2b50bd0dccaf02ae4c7c9df273608626e
junefish <junefish@users.noreply.github.com>
2022-12-01 21:18:41 -0500

    clean up comments and print statements

--- adventofcode2022/day1/day1problem1.py
+++ adventofcode2022/day1/day1problem1.py
@@ -1,14 +1,14 @@
+# initialise lists
 items = []
+elves = [[]]
+calories = []
 
+# read input file
 with open('adventofcode2022/day1/day1input.txt', 'r') as input:
     for line in input:
         items.append(line.strip())
-
-# print(items)
-# print(items[0])
-
-elves = [[]]
-
+        
+# assign items to elves
 delimiter = ''
 for item in items:
     if item == delimiter:
@@ -16,11 +16,9 @@ for item in items:
     elif item != delimiter: 
         elves[-1].append(int(item))
 
-# print(elves)
-
-calories = []
-
+# calculate calories carried per elf
 for elf in elves:
     calories.append(sum(elf))
 
+# print the max calories carried by an elf
 print(max(calories))
