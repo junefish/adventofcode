be78eeedf763e27d7bf3400069c66ebf363b714c
junefish <junefish@users.noreply.github.com>
2022-12-01 21:15:59 -0500

    update to use real input file

--- adventofcode2022/day1/day1problem1.py
+++ adventofcode2022/day1/day1problem1.py
@@ -1,6 +1,6 @@
 items = []
 
-with open('adventofcode2022/day1/day1example.txt', 'r') as input:
+with open('adventofcode2022/day1/day1input.txt', 'r') as input:
     for line in input:
         items.append(line.strip())
 
