9fde74258be591d2811271b8a287578673ae985a
junefish <junefish@users.noreply.github.com>
2022-12-02 13:24:37 -0500

    add scoring

--- adventofcode2022/day2/day2problem1.py
+++ adventofcode2022/day2/day2problem1.py
@@ -1,6 +1,42 @@
 rounds = []
 with open('adventofcode2022/day2/day2example.txt', 'r') as input:
     for line in input:
-        rounds.append(line.strip().split(' '))
+        round = (line.strip())
+        rounds.append(round.split(' '))
         
-print(rounds)
+# print(rounds)
+
+points = {'X': 1, 'Y': 2, 'Z': 3}
+
+# print(points['A'])
+
+outcomes = {'win': 6, 'draw': 3, 'loss': 0}
+
+scores = []
+
+for round in rounds:
+    if(round[0] == 'A'):
+        if(round[1] == 'X'):
+            result = outcomes['draw']
+        elif(round[1] == 'Y'):
+            result = outcomes['win']
+        elif(round[1] == 'Z'):
+            result = outcomes['loss']
+    elif(round[0] == 'B'):
+        if(round[1] == 'X'):
+            result = outcomes['loss']
+        elif(round[1] == 'Y'):
+            result = outcomes['draw']
+        elif(round[1] == 'Z'):
+            result = outcomes['win']
+    elif(round[0] == 'C'):
+        if(round[1] == 'X'):
+            result = outcomes['win']
+        elif(round[1] == 'Y'):
+            result = outcomes['loss']
+        elif(round[1] == 'Z'):
+            result = outcomes['draw']
+    score = result + points[round[1]]
+    scores.append(score)
+    
+print(sum(scores))
