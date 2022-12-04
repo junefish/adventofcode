9f7c431ee97abadcd9dd32ffb3dd891f2acdf5ac
junefish <junefish@users.noreply.github.com>
2022-12-04 16:55:09 -0500

    cleanup comments

--- adventofcode2022/day3/day3problem1.py
+++ adventofcode2022/day3/day3problem1.py
@@ -1,28 +1,30 @@
+# initialise lists
 contents = []
+common = []
+items = []
+
+# read input file
 with open('adventofcode2022/day3/day3input.txt', 'r') as input:
     for line in input:
         package = line.strip()
         first_half, second_half = package[:len(package)//2], package[len(package)//2:]
         contents.append([first_half,second_half])
 
-# print(contents)
-
-common = []
+# find items in common between compartments
 for rucksack in contents:
     common.append([value for value in rucksack[0] if value in rucksack[1]])
 
-items = []
+# reduce common items for uniqueness
 for c in common:
      items.append(''.join(set(c)))
 
-# print(items)
-
+# define priorities
 priorities = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
-# print(priorities)
-
+# find sum of priorities from common items
 total = 0
 for item in items:
     total = total + priorities.index(item) + 1
 
+# print answer
 print(total)
