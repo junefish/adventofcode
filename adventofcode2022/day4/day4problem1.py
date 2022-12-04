cf45f4b1e95a6451cabc1fd5bc31237847d42060
junefish <junefish@users.noreply.github.com>
2022-12-04 18:03:10 -0500

    fix comparison by casting to int

--- adventofcode2022/day4/day4problem1.py
+++ adventofcode2022/day4/day4problem1.py
@@ -15,6 +15,10 @@ for pair in assignments:
     start2,end2 = elf2.split('-')
     # print(start1,end1)
     # print(start2,end2)
+    start1 = int(start1)
+    start2 = int(start2)
+    end1 = int(end1)
+    end2 = int(end2)
     
     # check if elf 1's range contains elf 2's range
     if(start1 <= start2 and end1 >= end2):
