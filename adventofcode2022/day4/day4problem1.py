b798cd4b527efa6b5b1403b48d302a93e6cebc87
junefish <junefish@users.noreply.github.com>
2022-12-04 17:43:53 -0500

    calculate how many assigned ranges fully contain the other

--- adventofcode2022/day4/day4problem1.py
+++ adventofcode2022/day4/day4problem1.py
@@ -9,9 +9,18 @@ with open('adventofcode2022/day4/day4example.txt', 'r') as input:
 contains = 0
 for pair in assignments:
     elf1,elf2 = pair[0],pair[-1]
-    print(elf1,elf2)
+    # print(elf1,elf2)
     
     start1,end1 = elf1.split('-')
     start2,end2 = elf2.split('-')
-    print(start1,end1)
-    print(start2,end2)
+    # print(start1,end1)
+    # print(start2,end2)
+    
+    # check if elf 1's range contains elf 2's range
+    if(start1 <= start2 and end1 >= end2):
+        contains = contains + 1
+    # check if elf 2's range contains elf 1's range
+    elif(start2 <= start1 and end2 >= end1):
+        contains = contains + 1
+
+print(contains)
