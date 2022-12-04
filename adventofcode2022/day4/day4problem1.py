80716c9c91e8883fcb93ceb2cc5b3f4debfb747a
junefish <junefish@users.noreply.github.com>
2022-12-04 17:39:21 -0500

    split assignments into sections

--- adventofcode2022/day4/day4problem1.py
+++ adventofcode2022/day4/day4problem1.py
@@ -4,4 +4,14 @@ with open('adventofcode2022/day4/day4example.txt', 'r') as input:
         line = line.strip()
         assignments.append(line.split(','))
         
-print(assignments)
+# print(assignments)
+
+contains = 0
+for pair in assignments:
+    elf1,elf2 = pair[0],pair[-1]
+    print(elf1,elf2)
+    
+    start1,end1 = elf1.split('-')
+    start2,end2 = elf2.split('-')
+    print(start1,end1)
+    print(start2,end2)
