69857d508065f39f66e6cdb42c6806cdfd155028
junefish <junefish@users.noreply.github.com>
2022-12-04 00:18:21 -0500

    split contents into packages

--- adventofcode2022/day3/day3problem1.py
+++ adventofcode2022/day3/day3problem1.py
@@ -1,6 +1,8 @@
 contents = []
 with open('/data/data/com.termux/files/home/adventofcode/adventofcode2022/day3/day3example.txt', 'r') as input:
     for line in input:
-        contents.append(line.strip())
+        package = line.strip()
+        first_half, second_half = package[:len(package)//2], package[len(package)//2:]
+        contents.append([first_half,second_half])
 
 print(contents)
