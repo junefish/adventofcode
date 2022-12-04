25ffe744c6795f35eb9f42dac07f8dadaf6e407b
junefish <junefish@users.noreply.github.com>
2022-12-04 17:05:43 -0500

    cleanup comments

--- adventofcode2022/day3/day3problem2.py
+++ adventofcode2022/day3/day3problem2.py
@@ -1,32 +1,32 @@
+# initialise lists
 contents = []
+common = []
+items = []
+
+# read input file
 with open('adventofcode2022/day3/day3input.txt', 'r') as input:
     for line in input:
         contents.append(line.strip())
 
-# print(contents)
-
+# split contents into groups of 3
 group_size = 3
 groups = [contents[i:i + group_size] for i in range(0, len(contents), group_size)]
 
-# print(groups[0])
-# print(groups)
-
-common = []
+# find items in common within group members
 for rucksack in groups:
     common.append([value for value in rucksack[0] if value in rucksack[1] and value in rucksack[2]])
     
-# print(common)
-
-items = []
+# reduce to unique items
 for c in common:
      items.append(''.join(set(c)))
      
-print(items)
-
+# define priorities
 priorities = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
+# calculate sum of priorities for common items
 total = 0
 for item in items:
     total = total + priorities.index(item) + 1
 
+# print answer
 print(total)
