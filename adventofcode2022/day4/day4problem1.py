bd1a6a2ad95521f178752f16cc77413627b44786
junefish <junefish@users.noreply.github.com>
2022-12-04 17:31:56 -0500

    Update day4problem1.py

--- adventofcode2022/day4/day4problem1.py
+++ adventofcode2022/day4/day4problem1.py
@@ -1,7 +1,8 @@
-assignments = []
+elves = []
 with open('adventofcode2022/day4/day4example.txt', 'r') as input:
     for line in input:
         line = line.strip()
-        assignments.append(line.split(','))
+        elves.append(line.split(','))
         
-print(assignments)
+print(elves)
+print(elves[0])
