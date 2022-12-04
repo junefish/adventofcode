e3aa1306dc9e76503c18a537de81e1f7e5be3c50
junefish <junefish@users.noreply.github.com>
2022-12-04 16:59:55 -0500

    split bags into groups of 3 elves

--- adventofcode2022/day3/day3problem2.py
+++ adventofcode2022/day3/day3problem2.py
@@ -3,6 +3,12 @@ with open('adventofcode2022/day3/day3input.txt', 'r') as input:
     for line in input:
         contents.append(line.strip())
 
-print(contents)
+# print(contents)
+
+group_size = 3
+groups = [contents[i:i + group_size] for i in range(0, len(contents), group_size)]
+
+# print(groups[0])
+# print(groups)
 
 priorities = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
