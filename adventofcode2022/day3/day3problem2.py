53dd03e5b8340d314cc36e91937908e011cc2b98
junefish <junefish@users.noreply.github.com>
2022-12-04 17:02:26 -0500

    find items in common between group members

--- adventofcode2022/day3/day3problem2.py
+++ adventofcode2022/day3/day3problem2.py
@@ -11,4 +11,16 @@ groups = [contents[i:i + group_size] for i in range(0, len(contents), group_size
 # print(groups[0])
 # print(groups)
 
+common = []
+for rucksack in groups:
+    common.append([value for value in rucksack[0] if value in rucksack[1] and value in rucksack[2]])
+    
+# print(common)
+
+items = []
+for c in common:
+     items.append(''.join(set(c)))
+     
+print(items)
+
 priorities = list('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
