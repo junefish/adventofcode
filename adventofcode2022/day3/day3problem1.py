2472bda2a1f7b0c0c8fcef3b75e447d994384887
junefish <junefish@users.noreply.github.com>
2022-12-04 01:24:56 -0500

    switch to problem input

--- adventofcode2022/day3/day3problem1.py
+++ adventofcode2022/day3/day3problem1.py
@@ -1,5 +1,5 @@
 contents = []
-with open('/data/data/com.termux/files/home/adventofcode/adventofcode2022/day3/day3example.txt', 'r') as input:
+with open('/data/data/com.termux/files/home/adventofcode/adventofcode2022/day3/day3input.txt', 'r') as input:
     for line in input:
         package = line.strip()
         first_half, second_half = package[:len(package)//2], package[len(package)//2:]
