8ee4edc2ef1eea353ddcfee02f73c30fdbbe6615
junefish <junefish@users.noreply.github.com>
2022-12-06 00:50:06 -0500

    find first 14 unique characters

--- adventofcode2022/day6/day6problem2.py
+++ adventofcode2022/day6/day6problem2.py
@@ -4,8 +4,12 @@ with open('adventofcode2022/day6/day6example.txt', 'r') as file:
     for line in file:
         datastream = line.strip()
         
-for i in range(14, len(datastream)-14):
+for i in range(14, len(datastream)):
     marker = datastream[i-14:i]
     freq = Counter(marker)
 
-    print(marker, len(freq))
+    # print(marker, len(freq))
+    
+    if(len(freq) == 14):
+        print(i)
+        break
