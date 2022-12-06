e9b230cc11f0628fa99c64714f8ca45090e0c3a6
junefish <junefish@users.noreply.github.com>
2022-12-06 00:38:54 -0500

    iterate through datastream 4 characters at a time

--- adventofcode2022/day6/day6problem1.py
+++ adventofcode2022/day6/day6problem1.py
@@ -2,4 +2,8 @@ with open('adventofcode2022/day6/day6example.txt', 'r') as file:
     for line in file:
         datastream = line.strip()
         
-print(datastream)
+# print(datastream)
+
+for i in range(3, len(datastream)-3):
+    marker = datastream[i-4:i]
+    print(marker)
