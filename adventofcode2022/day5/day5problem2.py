3c21c6f7a835cefebf01d44d642cb379a5adac0b
junefish <junefish@users.noreply.github.com>
2022-12-06 22:50:12 -0500

    switch to problem input

--- adventofcode2022/day5/day5problem2.py
+++ adventofcode2022/day5/day5problem2.py
@@ -6,7 +6,7 @@ crate_stacks = []
 procedure = []
 
 # read input file
-with open('adventofcode2022/day5/day5example.txt', 'r') as file:
+with open('adventofcode2022/day5/day5input.txt', 'r') as file:
     for line in file:
         input.append(line)
 
