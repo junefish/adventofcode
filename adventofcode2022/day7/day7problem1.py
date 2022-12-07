60d2c6536d8772741ccb3a6cee2c6b24527b2c9e
junefish <junefish@users.noreply.github.com>
2022-12-07 00:35:02 -0500

    parse terminal output by type

--- adventofcode2022/day7/day7problem1.py
+++ adventofcode2022/day7/day7problem1.py
@@ -2,4 +2,19 @@ terminal_output = []
 with open('adventofcode2022/day7/day7example.txt', 'r') as file:
     for line in file:
         terminal_output.append(line.strip())
-print(terminal_output)
+# print(terminal_output)
+
+commands = []
+directories = []
+sizes = []
+for line in terminal_output:
+    output = line.split(' ')
+    if(line[0] == '$'):
+        commands.append(line.split('$ ')[-1])
+    elif(line[0] == 'd'):
+        directories.append(output[-1])
+    elif(line[0].isdigit()):
+        sizes.append(int(output[0]))
+print(commands)
+print(directories)
+print(sizes)
