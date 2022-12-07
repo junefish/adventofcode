f5b098c28386e02babf40ae8e47240cfb20f17ec
junefish <junefish@users.noreply.github.com>
2022-12-06 22:37:45 -0500

    Create day5problem2.py

---
+++ adventofcode2022/day5/day5problem2.py
@@ -0,0 +1,35 @@
+# initialise lists
+input = []
+instructions = [[]]
+crates = []
+crate_stacks = []
+procedure = []
+
+# read input file
+with open('adventofcode2022/day5/day5example.txt', 'r') as file:
+    for line in file:
+        input.append(line)
+
+# split instructions in half into drawing & procedure
+delimiter = '\n'
+for line in input:
+    if line == delimiter:
+        instructions.append([])
+    elif line != delimiter: 
+        instructions[-1].append(line.split('\n')[0])
+        
+# parse drawing into stacks of crates
+drawing = instructions[0]
+# parse row into crates
+for line in drawing:
+    crates.append([line[containers * 4 + 1] for containers in range(len(line) // 4 + 1)]) 
+# transpose rows into columns (stacks)
+crate_stacks = [list("".join(stack_column).strip()[::-1]) for stack_column in zip(*crates)]
+
+# clean up steps into array
+for step in instructions[1]:
+    step = step.split(' ')
+    step.remove('move')
+    step.remove('from')
+    step.remove('to')
+    procedure.append(step)
