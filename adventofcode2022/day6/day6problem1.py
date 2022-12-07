c92c5203609ff9a7643f25b70f4c58eca944afcd
junefish <junefish@users.noreply.github.com>
2022-12-06 22:52:44 -0500

    cleanup comments

--- adventofcode2022/day6/day6problem1.py
+++ adventofcode2022/day6/day6problem1.py
@@ -1,15 +1,18 @@
+# import modules
 from collections import Counter
 
+# read input file
 with open('adventofcode2022/day6/day6input.txt', 'r') as file:
     for line in file:
         datastream = line.strip()
-        
-# print(datastream)
 
+# find marker with 4 unique characters
 for i in range(3, len(datastream)-3):
+    # check character frequency in marker
     marker = datastream[i-4:i]
     freq = Counter(marker)
     
+    # print first marker with 4 unique characters
     if(len(freq) == 4):
         print(i)
         break
