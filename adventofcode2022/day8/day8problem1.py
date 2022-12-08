ec35b28f8e4c2364502f3c117c8ba71743f64e70
junefish <junefish@users.noreply.github.com>
2022-12-08 00:31:36 -0500

    rename matrix for clarity

--- adventofcode2022/day8/day8problem1.py
+++ adventofcode2022/day8/day8problem1.py
@@ -1,8 +1,8 @@
-trees = []
+forest = []
 with open('adventofcode2022/day8/day8example.txt', 'r') as file:
     for line in file:
         treeline = (line.strip())
-        trees.append([*treeline])
-    trees = [list(map(int, x)) for x in trees]
+        forest.append([*treeline])
+    forest = [list(map(int, x)) for x in forest]
  
-print(trees)
+print(forest)
