a3b68c4c413b2d88914bdd82d84fa95e746e4ff2
junefish <junefish@users.noreply.github.com>
2022-12-08 23:40:05 -0500

    calculate space info

--- adventofcode2022/day7/day7problem2.py
+++ adventofcode2022/day7/day7problem2.py
@@ -30,3 +30,12 @@ for line in terminal_output:
             size = int(size)
             for i in range(len(filepath)):
                 sizes['/'.join(filepath[:i+1])] += size
+                
+total_space = 70000000
+update_size = 30000000
+used_space = sizes['/']
+print(used_space)
+free_space = total_space - used_space
+print(free_space)
+space_needed = update_size - free_space
+print(space_needed)
