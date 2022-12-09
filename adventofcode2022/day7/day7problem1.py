c5ccdf8c92fc63fd901c5faa1052a731bf433327
junefish <junefish@users.noreply.github.com>
2022-12-08 23:29:49 -0500

    switch to problem input

--- adventofcode2022/day7/day7problem1.py
+++ adventofcode2022/day7/day7problem1.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 
 terminal_output = []
-with open('adventofcode2022/day7/day7example.txt', 'r') as file:
+with open('adventofcode2022/day7/day7input.txt', 'r') as file:
     for line in file:
         terminal_output.append(line.strip())
 # print(terminal_output)
