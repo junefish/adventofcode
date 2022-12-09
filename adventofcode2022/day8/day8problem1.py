f5763951f1170d94e71ef30fb9a5569687a99f2f
junefish <junefish@users.noreply.github.com>
2022-12-09 00:01:21 -0500

    cleanup comments

--- adventofcode2022/day8/day8problem1.py
+++ adventofcode2022/day8/day8problem1.py
@@ -1,20 +1,17 @@
+# initialise variables
 forest = []
+# tranpose forest for easier parsing
+forest2 = list(zip(*forest))
+visible = 0
+
+# read input file
 with open('adventofcode2022/day8/day8input.txt', 'r') as file:
     for line in file:
         treeline = (line.strip())
         forest.append([*treeline])
     forest = [list(map(int, x)) for x in forest]
- 
-# print(forest)
 
-# # trees around the edge of the grid
-# visible = (len(forest)-1)*4
-# print(visible)
-
-# tranpose forest for easier parsing
-forest2 = list(zip(*forest))
-
-visible = 0
+# count how many trees are visible
 for i in range(len(forest[0])):
     for j in range(len(forest)):
         tree = forest[i][j]
@@ -23,5 +20,6 @@ for i in range(len(forest[0])):
             all(x < tree for x in forest2[j][0:i]) or \
             all(x < tree for x in forest2[j][i+1:]):
             visible += 1
-            
+
+# print answer
 print(visible)
