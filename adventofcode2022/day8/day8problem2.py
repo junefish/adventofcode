e528193ba4aacbfe9a69eeac3eeec11d994cd323
junefish <junefish@users.noreply.github.com>
2022-12-09 00:36:35 -0500

    calculate view lengths

--- adventofcode2022/day8/day8problem2.py
+++ adventofcode2022/day8/day8problem2.py
@@ -7,3 +7,31 @@ with open('adventofcode2022/day8/day8example.txt', 'r') as file:
         treeline = (line.strip())
         forest.append([*treeline])
     forest = [list(map(int, x)) for x in forest]
+
+for i in range(len(forest[0])):
+    for j in range(len(forest)):
+        tree = forest[i][j]
+        
+        left = 0
+        for t in forest[i][0:j][::-1]:
+            left += 1
+            if t >= tree:
+                break
+        
+        right = 0
+        for t in forest[i][j+1:]:
+            right += 1
+            if t >= tree:
+                break
+
+        up = 0
+        for t in forest2[j][0:i][::-1]:
+            up += 1
+            if t >= tree:
+                break
+            
+        down = 0
+        for t in forest2[j][i+1:]:
+            up += 1
+            if t >= tree:
+                break
