50a08606376ed9eb65f4cf7dc7d709b1a89f6983
junefish <junefish@users.noreply.github.com>
2022-12-08 23:59:37 -0500

    change to use transposed forest to check all trees

--- adventofcode2022/day8/day8problem1.py
+++ adventofcode2022/day8/day8problem1.py
@@ -7,6 +7,21 @@ with open('adventofcode2022/day8/day8example.txt', 'r') as file:
  
 # print(forest)
 
-# trees around the edge of the grid
-visible = (len(forest)-1)*4
+# # trees around the edge of the grid
+# visible = (len(forest)-1)*4
+# print(visible)
+
+# tranpose forest for easier parsing
+forest2 = list(zip(*forest))
+
+visible = 0
+for i in range(len(forest[0])):
+    for j in range(len(forest)):
+        tree = forest[i][j]
+        if all(x < tree for x in forest[i][0:j]) or \
+            all(x < tree for x in forest[i][j+1:]) or \
+            all(x < tree for x in forest2[j][0:i]) or \
+            all(x < tree for x in forest2[j][i+1:]):
+            visible += 1
+            
 print(visible)
