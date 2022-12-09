41a6f55cfb7fd26399ff584828b8a98dfc379182
junefish <junefish@users.noreply.github.com>
2022-12-08 23:44:42 -0500

    switch to problem input

--- adventofcode2022/day7/day7problem2.py
+++ adventofcode2022/day7/day7problem2.py
@@ -4,7 +4,7 @@ terminal_output = []
 filepath = []
 sizes = defaultdict(int)
 
-with open('adventofcode2022/day7/day7example.txt', 'r') as file:
+with open('adventofcode2022/day7/day7input.txt', 'r') as file:
     for line in file:
         terminal_output.append(line.strip())
 
