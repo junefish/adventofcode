5fb08b4bacd4d19434651eec6ddc77b93aca10d9
junefish <junefish@users.noreply.github.com>
2022-12-10 18:42:24 -0500

    Create day10problem2.py

---
+++ adventofcode2022/day10/day10problem2.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+signal = []
+with open('adventofcode2022/day10/day10example2.txt', 'r') as file:
+    for line in file:
+        signal.append(line.strip())
+
+cycles = defaultdict(int)
+X = 1
+cycle = 1
+for command in signal:
+    cycles[cycle] = X
+    if(command=='noop'):
+        cycle += 1
+        cycles[cycle] = X
+    elif(command.startswith('addx')):
+        V = int(command.split(' ')[-1])
+        for i in range(2):
+            cycle += 1
+            cycles[cycle] = X
+        X += V
+        cycles[cycle] = X
+
+for(cycle,X) in cycles.items():
+    print(cycle,X)
