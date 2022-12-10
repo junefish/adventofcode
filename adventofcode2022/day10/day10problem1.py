cdf396379d3cd8fef0167b300a9a3ea0779f1cf2
junefish <junefish@users.noreply.github.com>
2022-12-10 18:03:30 -0500

    calculate values to be added

--- adventofcode2022/day10/day10problem1.py
+++ adventofcode2022/day10/day10problem1.py
@@ -3,4 +3,11 @@ with open('adventofcode2022/day10/day10example1.txt', 'r') as file:
     for line in file:
         signal.append(line.strip())
         
-print(signal)
+# print(signal)
+
+for command in signal:
+    if(command=='noop'):
+        continue
+    elif(command.startswith('addx')):
+        V = int(command.split(' ')[-1])
+        print(V)
