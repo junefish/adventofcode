073eb4fd6829227b832faae43c19da6b316bcbb1
junefish <junefish@users.noreply.github.com>
2022-12-10 22:36:12 -0500

    calculate movements of head

--- adventofcode2022/day9/day9problem1.py
+++ adventofcode2022/day9/day9problem1.py
@@ -4,4 +4,28 @@ with open('adventofcode2022/day9/day9example.txt', 'r') as file:
     for line in file:
         dir, dist = line.strip().split()
         movements.append((dir,int(dist)))
-print(movements)
+# print(movements)
+
+# define H as bottom left corner of grid
+H = {'x': 0, 'y': 0}
+
+for move in movements:
+    dir,dist = move
+    
+    # move left
+    if(dir == 'L'):
+        H['x'] -= dist
+        
+    # move right
+    elif(dir == 'R'):
+        H['x'] += dist
+    
+    # move up
+    elif(dir == 'U'):
+        H['y'] += dist
+    
+    # move down
+    elif(dir == 'D'):
+        H['y'] -= dist
+    
+    print(H['x'],H['y'])
