85c4a424b8aefbe32219a3898145a9b2058b7925
junefish <junefish@users.noreply.github.com>
2022-12-10 23:17:35 -0500

    find stepwise positions for H (not working)

--- adventofcode2022/day9/day9problem1.py
+++ adventofcode2022/day9/day9problem1.py
@@ -9,23 +9,49 @@ with open('adventofcode2022/day9/day9example.txt', 'r') as file:
 # define H as bottom left corner of grid
 H = {'x': 0, 'y': 0}
 
+H_positions = []
 for move in movements:
     dir,dist = move
     
     # move left
     if(dir == 'L'):
-        H['x'] -= dist
+        Hx = H['x'] - dist
+        Hy = H['y']
         
     # move right
     elif(dir == 'R'):
-        H['x'] += dist
+        Hx = H['x'] + dist
+        Hy = H['y']
+        
+    # increment H stepwise when moving L/R
+    if(H['y'] == Hy):
+        while(H['x'] != Hx):
+            H_positions.append((H['x'],H['y']))
+            # decrease by 1 if moving L
+            if(H['x'] > Hx):
+                H['x'] -= 1
+            # increase by 1 if moving R
+            if(H['x'] < Hx):
+                H['x'] += 1
     
     # move up
     elif(dir == 'U'):
-        H['y'] += dist
+        Hx = H['x']
+        Hy = H['y'] + dist
     
     # move down
     elif(dir == 'D'):
-        H['y'] -= dist
+        Hx = H['x']
+        Hy = H['y'] - dist
     
-    print(H['x'],H['y'])
+    # increment H stepwise when moving U/D
+    if(H['x'] == Hx):
+        while(H['y'] != Hy):
+            H_positions.append((H['x'],H['y']))
+            # increase by 1 if moving U
+            if(H['y'] < Hy):
+                H['y'] += 1
+            # decrease by 1 if moving D
+            if(H['y'] > Hy):
+                H['y'] -= 1
+print(H_positions)
