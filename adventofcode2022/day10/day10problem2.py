e56d06f02e43c80fd25d7fbab549b68162ea2d1a
junefish <junefish@users.noreply.github.com>
2022-12-10 19:07:05 -0500

    cleanup comments

--- adventofcode2022/day10/day10problem2.py
+++ adventofcode2022/day10/day10problem2.py
@@ -1,18 +1,32 @@
+# import modules
 from collections import defaultdict
 
+# initialise lists
 signal = []
+cycles = defaultdict(int)
+sprites = []
+
+# define variables
+X = 1
+cycle = 1
+output = ''
+screen_width = 40
+
+# read input file
 with open('adventofcode2022/day10/day10input.txt', 'r') as file:
     for line in file:
         signal.append(line.strip())
 
-cycles = defaultdict(int)
-X = 1
-cycle = 1
+# run through commands in program
 for command in signal:
     cycles[cycle] = X
+    
+    # noop = continue
     if(command=='noop'):
         cycle += 1
         cycles[cycle] = X
+    
+    # addx V = wait 2 cycles then add V to X
     elif(command.startswith('addx')):
         V = int(command.split(' ')[-1])
         for i in range(2):
@@ -21,18 +35,23 @@ for command in signal:
         X += V
         cycles[cycle] = X
 
-sprites = []
+# save X values as sprites
 for(cycle,X) in cycles.items():
-#     print(cycle,X)
     sprites.append(X)
 
-output = ''
-screen_width = 40
+# set CRT screen
 for i, value in enumerate(sprites):
+    # light pixel if pixel is in range of 3-pixel sprite
     if sprites[i] in range((i % screen_width) - 1, (i % screen_width) + 2):
         output += "#"
+
+    # otherwise keep pixel dark
     else:
         output += "."
+    
+    # at edge of screen add newline
     if (i + 1) % screen_width == 0:
         output += "\n"
+
+# print answer
 print(output)
