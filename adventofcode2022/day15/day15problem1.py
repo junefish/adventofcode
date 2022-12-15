7ce4d022aff3191632da3ef49dd2c034a3a10bb6
junefish <junefish@users.noreply.github.com>
2022-12-15 16:25:50 -0500

    strip unnecessary input from sensors

--- adventofcode2022/day15/day15problem1.py
+++ adventofcode2022/day15/day15problem1.py
@@ -7,5 +7,13 @@ with open('adventofcode2022/day15/day15input.txt', 'r') as input:
         sensors.append(s.strip())
         beacons.append(b.strip())
         
+# print(sensors)
+# print(beacons)
+
+for sensor in sensors:
+    sensors.remove(sensor)
+    s = sensor.replace('Sensor at ', '')
+    sensors.append(s)
+
+sensors = [item for item in sensors if not item.startswith('S')]
 print(sensors)
-print(beacons)
