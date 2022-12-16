0ba56dfa021a41ba8ad5e10e4c89945b56b46885
junefish <junefish@users.noreply.github.com>
2022-12-16 00:02:59 -0500

    find destination coordinates

--- adventofcode2022/day12/day12problem1.py
+++ adventofcode2022/day12/day12problem1.py
@@ -3,4 +3,12 @@ with open('adventofcode2022/day12/day12example.txt', 'r') as input:
     for line in input:
         x = [*line.strip()]
         heightmap.append(x)
-print(heightmap)
+# print(heightmap)
+
+start = (0,0)
+for i,_ in enumerate(heightmap):
+    for j,_ in enumerate(heightmap[0]):
+        if(heightmap[i][j] == 'E'):
+            end = (i,j)
+            break
+print(end)
