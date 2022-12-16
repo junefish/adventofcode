e70a2d41a39a9600d60b2de03e40829d3c937fe4
junefish <junefish@users.noreply.github.com>
2022-12-16 01:28:16 -0500

    switch to problem input

--- adventofcode2022/day12/day12problem1.py
+++ adventofcode2022/day12/day12problem1.py
@@ -1,5 +1,5 @@
 input = []
-with open('adventofcode2022/day12/day12example.txt', 'r') as file:
+with open('adventofcode2022/day12/day12input.txt', 'r') as file:
     for line in file:
         x = [*line.strip()]
         input.append(x)
