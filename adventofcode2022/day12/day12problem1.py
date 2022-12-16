988b9765b34904ac412bbfe7404d8fa64c64c481
junefish <junefish@users.noreply.github.com>
2022-12-16 01:32:21 -0500

    cleanup comments

--- adventofcode2022/day12/day12problem1.py
+++ adventofcode2022/day12/day12problem1.py
@@ -1,10 +1,20 @@
+# initialise lists
 input = []
+heightmap = []
+
+# read input file
 with open('adventofcode2022/day12/day12input.txt', 'r') as file:
     for line in file:
         x = [*line.strip()]
         input.append(x)
-# print(input)
 
+# replace start with 'a' and end with 'z' for unicode parsing later
+for line in input:
+    row = list(map(lambda item: item.replace('S','a'), line))
+    row2 = list(map(lambda item: item.replace('E','z'), row))
+    heightmap.append(row2)
+
+# function to find coordinates of starting and destination positions
 def findCoords(map):
     for i,_ in enumerate(map):
         for j,_ in enumerate(map[0]):
@@ -13,18 +23,8 @@ def findCoords(map):
             if(map[i][j] == 'E'):
                 end = (i,j)
     return start, end
-start, end = findCoords(input)
-# print(start, end)
-
-# replace start with 'a' and end with 'z' for unicode parsing later
-heightmap = []
-for line in input:
-    row = list(map(lambda item: item.replace('S','a'), line))
-    row2 = list(map(lambda item: item.replace('E','z'), row))
-    heightmap.append(row2)
-# print(heightmap)
     
-# find adjacent points
+# function to find adjacent points
 def findNeighbors(map, point):
     x_min,y_min = 0,0
     x_max = len(map) - 1
@@ -54,18 +54,24 @@ def findNeighbors(map, point):
         
     return(neighbors)
 
+# function to search matrix from end to start
 def search(map,end):
     mountains = [end]
     level = {}
     level[end] = 0
     
+    # search the map
     while mountains:
         current = mountains.pop(0)
         for neighbor in findNeighbors(map, current):
             if neighbor not in level:
+                # increment path length
                 level[neighbor] = level[current] + 1
                 mountains.append(neighbor)
-
     return level
 
+# find coordinates of starting and destination positions
+start, end = findCoords(input)
+
+# print answer
 print(search(heightmap,end)[start])
