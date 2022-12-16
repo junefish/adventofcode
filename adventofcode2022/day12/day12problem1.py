b308377d6368b340003214a48fb72e8d2472a157
junefish <junefish@users.noreply.github.com>
2022-12-16 01:16:12 -0500

    fix variable call in findNeighbors function

--- adventofcode2022/day12/day12problem1.py
+++ adventofcode2022/day12/day12problem1.py
@@ -21,29 +21,29 @@ for row in heightmap:
 # find adjacent points
 def findNeighbors(map, point):
     x_min,y_min = 0,0
-    x_max = len(heightmap) - 1
-    y_max = len(heightmap[0]) - 1
+    x_max = len(map) - 1
+    y_max = len(map[0]) - 1
     x, y = point
 
     # -2 allowing only single steps down the mountain
-    node_height = ord(heightmap[x][y]) - 2
+    node_height = ord(map[x][y]) - 2
 
     neighbors = []
 
     # ensure X isn't an edge and left isn't lower than - 1 height
-    if (x_min < x) and (node_height < ord(heightmap[x - 1][y])):
+    if (x_min < x) and (node_height < ord(map[x - 1][y])):
         neighbors.append((x - 1, y))
 
     # same for right
-    if (x < x_max) and (node_height < ord(heightmap[x + 1][y])):
+    if (x < x_max) and (node_height < ord(map[x + 1][y])):
         neighbors.append((x + 1, y))
 
     # same for top
-    if (y_min < y) and (node_height < ord(heightmap[x][y - 1])):
+    if (y_min < y) and (node_height < ord(map[x][y - 1])):
         neighbors.append((x, y - 1))
 
     # same for bottom
-    if (y < y_max) and (node_height < ord(heightmap[x][y + 1])):
+    if (y < y_max) and (node_height < ord(map[x][y + 1])):
         neighbors.append((x, y + 1))
         
     return(neighbors)
