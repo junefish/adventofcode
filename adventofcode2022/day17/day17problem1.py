2fede2c5bf95df8bad4e3f886d948fc0153e380a
junefish <junefish@users.noreply.github.com>
2022-12-17 00:49:10 -0500

    add rock shapes

--- adventofcode2022/day17/day17problem1.py
+++ adventofcode2022/day17/day17problem1.py
@@ -3,4 +3,40 @@ with open('adventofcode2022/day17/day17example.txt') as input:
     for line in input:
         for char in line:
             jets.append(char)
-print(jets)
+# print(jets)
+
+rocks = []
+"""
+first rock:
+    #### 
+"""
+rocks.append([(0,0),(0,1),(0,2),(0,3)])
+"""
+second rock
+    .#.
+    ###
+    .#.
+"""
+rocks.append([(0,1),(1,0),(1,1),(1,2),(2,1)])
+"""
+third rock
+    ..#
+    ..#
+    ###
+"""
+rocks.append([(0,0),(0,1),(0,2),(1,2),(2,2)])
+"""
+fourth rock:
+    #
+    #
+    #
+    #
+"""
+rocks.append([(0,0),(1,0),(2,0),(3,0)])
+"""
+fifth rock:
+    ##
+    ##
+"""
+rocks.append([(0,0),(1,0),(1,0),(1,1)])
+print(rocks)
