c46742102bbde29a2be5497577dda690d139c7c8
junefish <junefish@users.noreply.github.com>
2022-12-21 00:27:26 -0500

    Create day18problem1.py

---
+++ adventofcode2022/day18/day18problem1.py
@@ -0,0 +1,5 @@
+cubes = []
+with open('adventofcode2022/day18/day18example.txt') as input:
+    for line in input:
+        cubes.append([int(i) for i in line.strip().split(',')])
+print(cubes)        
