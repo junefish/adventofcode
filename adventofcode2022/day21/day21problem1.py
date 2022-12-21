8e1cd474c52135798c5dd6a8eb4fb7838c61152a
junefish <junefish@users.noreply.github.com>
2022-12-21 01:01:58 -0500

    Create day21problem1.py

---
+++ adventofcode2022/day21/day21problem1.py
@@ -0,0 +1,14 @@
+monkeys = {}
+with open('adventofcode2022/day21/day21example.txt') as input:
+    for line in input:
+        name = line.strip().split(': ')[0]
+        info = line.strip().split(': ')[1]
+        
+        if info.isnumeric():
+            i = eval(info)
+        else:
+            i = info.split(' ')
+        
+        monkeys.update({name: i})
+print(monkeys)
+print(monkeys['root'])
