8fc5910af2fc041ebc053e4e1508c54e75138c24
junefish <junefish@users.noreply.github.com>
2022-12-25 02:55:58 -0500

    Create day25problem1.py

---
+++ adventofcode2022/day25/day25problem1.py
@@ -0,0 +1,11 @@
+requirements = [[]]
+with open('adventofcode2022/day25/day25example.txt') as file:
+    for line in file:
+        for char in line:
+            if char == '\n':
+                requirements.append([])
+            elif not char.isnumeric():
+                requirements[-1].append(char)
+            else:
+                requirements[-1].append(eval(char))
+print(requirements)
