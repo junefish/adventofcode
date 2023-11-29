faab59e70d804eceab5654d208b5a4b8ee2b8765
junefish <junefish@users.noreply.github.com>
2023-11-29 14:19:09 -0500

    Create day1problem1.py

---
+++ adventofcode2015/day1/day1problem1.py
@@ -0,0 +1,8 @@
+instructions = []
+
+# read input file
+with open('adventofcode2015/day1/day1input.txt', 'r') as input:
+    for line in input:
+        instructions.append(line.strip())
+
+print(instructions)
