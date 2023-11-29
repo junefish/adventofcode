8145260587536202e222299ee585e65e8f5972d1
junefish <junefish@users.noreply.github.com>
2023-11-29 14:47:03 -0500

    Create day2problem2.py

---
+++ adventofcode2015/day2/day2problem2.py
@@ -0,0 +1,11 @@
+dimensions = []
+
+# read input file
+with open('adventofcode2015/day2/day2example.txt', 'r') as input:
+    for line in input:
+        dimensions.append((line.strip().split('x')))
+
+for present in dimensions:
+    length = int(present[0])
+    width = int(present[1])
+    height = int(present[2])
