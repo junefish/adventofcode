d797aabbd615bfa6fd762033db234ef132c616c0
junefish <junefish@users.noreply.github.com>
2023-11-29 14:51:55 -0500

    calculate total ribbon length

--- adventofcode2015/day2/day2problem2.py
+++ adventofcode2015/day2/day2problem2.py
@@ -5,6 +5,7 @@ with open('adventofcode2015/day2/day2example.txt', 'r') as input:
     for line in input:
         dimensions.append((line.strip().split('x')))
 
+ribbon = 0
 for present in dimensions:
     length = int(present[0])
     width = int(present[1])
@@ -12,6 +13,9 @@ for present in dimensions:
 
     sides = [length, width, height]
     wrap = 2*sorted(sides)[0] + 2*sorted(sides)[1]
-    print(wrap)
 
     bow = length*width*height
+
+    ribbon += wrap + bow
+
+print(ribbon)
