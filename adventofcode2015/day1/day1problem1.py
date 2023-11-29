c869f681a677791c98aa8c1b89eb0c83c8c93c7c
junefish <junefish@users.noreply.github.com>
2023-11-29 14:28:19 -0500

    update reading input

--- adventofcode2015/day1/day1problem1.py
+++ adventofcode2015/day1/day1problem1.py
@@ -3,4 +3,5 @@ instructions = []
 # read input file
 with open('adventofcode2015/day1/day1example.txt', 'r') as input:
     for line in input:
-        instructions.append(list(line.strip()))
+        instructions += line.strip()
+    print(instructions)
