e71e3b23e1f8a61b2e5c67db527390f7bb356850
junefish <junefish@users.noreply.github.com>
2023-11-29 14:28:41 -0500

    check input character

--- adventofcode2015/day1/day1problem1.py
+++ adventofcode2015/day1/day1problem1.py
@@ -5,3 +5,9 @@ with open('adventofcode2015/day1/day1example.txt', 'r') as input:
     for line in input:
         instructions += line.strip()
     print(instructions)
+
+for char in instructions:
+    if(char == '('):
+        print(char + " is an open paren")
+    elif(char == ')'):
+        print(char + " is a closed paren")
