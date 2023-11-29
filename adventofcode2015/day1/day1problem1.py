105cf0051511c37e5f8dab04543250fab760e86b
junefish <junefish@users.noreply.github.com>
2023-11-29 14:29:32 -0500

    change to problem input

--- adventofcode2015/day1/day1problem1.py
+++ adventofcode2015/day1/day1problem1.py
@@ -1,7 +1,7 @@
 instructions = []
 
 # read input file
-with open('adventofcode2015/day1/day1example.txt', 'r') as input:
+with open('adventofcode2015/day1/day1input.txt', 'r') as input:
     for line in input:
         instructions += line.strip()
 
