619b318c6e37199e941f1eb741f79f4de8ab7a5a
junefish <junefish@users.noreply.github.com>
2023-11-29 14:38:53 -0500

    calculate surface area

--- adventofcode2015/day2/day2problem1.py
+++ adventofcode2015/day2/day2problem1.py
@@ -4,4 +4,11 @@ dimensions = []
 with open('adventofcode2015/day2/day2example.txt', 'r') as input:
     for line in input:
         dimensions.append((line.strip().split('x')))
-print(dimensions)
+
+for present in dimensions:
+    length = int(present[0])
+    width = int(present[1])
+    height = int(present[2])
+
+    area = 2*length*width + 2*width*height + 2*height*length
+    print(area)
