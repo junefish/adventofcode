f7f74863a8b8b5098ab1cb164c09cc8abb5c4e6f
junefish <junefish@users.noreply.github.com>
2023-11-29 14:23:58 -0500

    split input into characters

--- adventofcode2015/day1/day1problem1.py
+++ adventofcode2015/day1/day1problem1.py
@@ -3,4 +3,4 @@ instructions = []
 # read input file
 with open('adventofcode2015/day1/day1example.txt', 'r') as input:
     for line in input:
-        instructions.append(line.strip())
+        instructions.append(list(line.strip()))
