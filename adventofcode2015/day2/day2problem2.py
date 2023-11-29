40e5efdc15d22e9275f58968f8cf7b6cb16211ba
junefish <junefish@users.noreply.github.com>
2023-11-29 14:55:07 -0500

    change to problem input

--- adventofcode2015/day2/day2problem2.py
+++ adventofcode2015/day2/day2problem2.py
@@ -1,7 +1,7 @@
 dimensions = []
 
 # read input file
-with open('adventofcode2015/day2/day2example.txt', 'r') as input:
+with open('adventofcode2015/day2/day2input.txt', 'r') as input:
     for line in input:
         dimensions.append((line.strip().split('x')))
 
