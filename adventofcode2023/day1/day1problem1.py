c00a8c6a3a94268cde37ede46327512a756950f4
junefish <junefish@users.noreply.github.com>
2023-12-01 00:34:58 -0500

    switch to problem input

--- adventofcode2023/day1/day1problem1.py
+++ adventofcode2023/day1/day1problem1.py
@@ -1,7 +1,7 @@
 calibrations = []
 
 # read input file
-with open('adventofcode2023/day1/day1example.txt', 'r') as input:
+with open('adventofcode2023/day1/day1input.txt', 'r') as input:
     for line in input:
         tmp = ""
         for char in line.strip():
