d7fca3af33a2e4392a2d496f85b0bc8fe6e3606a
junefish <junefish@users.noreply.github.com>
2023-12-01 00:59:15 -0500

    check if spelled-out numbers are in strings

--- adventofcode2023/day1/day1problem2.py
+++ adventofcode2023/day1/day1problem2.py
@@ -5,4 +5,21 @@ with open('adventofcode2023/day1/day1example2.txt', 'r') as input:
     for line in input:
         calibrations.append(line.strip())
 
-print(calibrations)
+# print(calibrations)
+
+digits = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+for value in calibrations:
+    for key,num in digits.items():
+        if(key in value):
+            print(key)
