2e3adb545080930dfb4f420f29b0eca452ba9f1a
junefish <junefish@users.noreply.github.com>
2023-12-01 00:29:35 -0500

    filter out non-numberic characters

--- adventofcode2023/day1/day1problem1.py
+++ adventofcode2023/day1/day1problem1.py
@@ -3,6 +3,10 @@ calibrations = []
 # read input file
 with open('adventofcode2023/day1/day1example.txt', 'r') as input:
     for line in input:
-        calibrations.append(line.strip())
+        tmp = ""
+        for char in line.strip():
+            if(char.isnumeric()):
+                tmp += char
+        calibrations.append(tmp)
         
 print(calibrations)
