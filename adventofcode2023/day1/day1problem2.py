ecdc091870f79681ae85d7da8846325ca7680df0
junefish <junefish@users.noreply.github.com>
2023-12-01 00:46:26 -0500

    Create day1problem2.py

---
+++ adventofcode2023/day1/day1problem2.py
@@ -0,0 +1,8 @@
+calibrations = []
+
+# read input file
+with open('adventofcode2023/day1/day1example2.txt', 'r') as input:
+    for line in input:
+        calibrations.append(line.strip())
+
+print(calibrations)
