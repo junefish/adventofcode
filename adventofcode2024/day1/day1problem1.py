51a2cd4e77b0948ab9d0b8ccd1c5acc11844f4de
junefish <junefish@users.noreply.github.com>
2024-12-02 15:09:58 -0500

    Create day1problem1.py

---
+++ adventofcode2024/day1/day1problem1.py
@@ -0,0 +1,10 @@
+left = []
+right = []
+
+with open("adventofcode2024/day1/day1example.txt", "r") as input:
+    for line in input:
+        left.append(int(line.strip().split(" ")[0]))
+        right.append(int(line.strip().split(" ")[-1]))
+
+print(left)
+print(right)
