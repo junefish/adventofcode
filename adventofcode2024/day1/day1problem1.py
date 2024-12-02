408ac6979d65f418e2fa0fb5a8e014ac65fbe854
junefish <junefish@users.noreply.github.com>
2024-12-02 15:21:18 -0500

    create find_distance function

--- adventofcode2024/day1/day1problem1.py
+++ adventofcode2024/day1/day1problem1.py
@@ -6,5 +6,19 @@ with open("adventofcode2024/day1/day1example.txt", "r") as input:
         left.append(int(line.strip().split(" ")[0]))
         right.append(int(line.strip().split(" ")[-1]))
 
-print(left)
-print(right)
+
+def find_distance(list1, list2):
+    if len(list1) != len(list2):
+        result = "Lists are not the same length"
+    else:
+        for i in range(len(list1)):
+            print(
+                str(list1[i])
+                + " - "
+                + str(list2[i])
+                + " = "
+                + str(abs(list1[i] - list2[i]))
+            )
+
+
+find_distance(sorted(left), sorted(right))
