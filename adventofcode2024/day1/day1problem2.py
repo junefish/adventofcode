51da9670c1703ff6edb90362b3fad51eb31372e9
junefish <junefish@users.noreply.github.com>
2024-12-02 15:33:18 -0500

    create similarity_score function

--- adventofcode2024/day1/day1problem2.py
+++ adventofcode2024/day1/day1problem2.py
@@ -5,3 +5,14 @@ with open("adventofcode2024/day1/day1example.txt", "r") as input:
     for line in input:
         left.append(int(line.strip().split(" ")[0]))
         right.append(int(line.strip().split(" ")[-1]))
+
+
+def similarity_score(list1, list2):
+    scores = []
+    for item in list1:
+        count = list2.count(item)
+        scores.append(item * count)
+    return scores
+
+
+print(similarity_score(left, right))
