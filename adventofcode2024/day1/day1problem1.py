aa01aa13b8fd44ad5320084aea42152cc0e2b7e9
junefish <junefish@users.noreply.github.com>
2024-12-02 15:23:33 -0500

    switch to problem input

--- adventofcode2024/day1/day1problem1.py
+++ adventofcode2024/day1/day1problem1.py
@@ -1,7 +1,7 @@
 left = []
 right = []
 
-with open("adventofcode2024/day1/day1example.txt", "r") as input:
+with open("adventofcode2024/day1/day1input.txt", "r") as input:
     for line in input:
         left.append(int(line.strip().split(" ")[0]))
         right.append(int(line.strip().split(" ")[-1]))
