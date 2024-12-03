c2e80ce2876240d33e93aa7584ada5c1e635e470
junefish <junefish@users.noreply.github.com>
2024-12-03 15:13:28 -0500

    switch to problem input

--- adventofcode2024/day2/day2problem1.py
+++ adventofcode2024/day2/day2problem1.py
@@ -1,5 +1,5 @@
 reports = []
-with open("adventofcode2024/day2/day2example.txt", "r") as input:
+with open("adventofcode2024/day2/day2input.txt", "r") as input:
     for line in input:
         reports.append([int(item) for item in line.strip().split(" ")])
 
