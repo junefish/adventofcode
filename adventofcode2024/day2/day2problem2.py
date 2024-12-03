057c725aeb07be296e696f3e89e82686692b5b47
junefish <junefish@users.noreply.github.com>
2024-12-03 15:42:16 -0500

    record unsafe levels

--- adventofcode2024/day2/day2problem2.py
+++ adventofcode2024/day2/day2problem2.py
@@ -3,4 +3,25 @@ with open("adventofcode2024/day2/day2example.txt", "r") as input:
     for line in input:
         reports.append([int(item) for item in line.strip().split(" ")])
 
-print(reports)
+
+def check_safety(list):
+    # check adjacent levels difference
+    if all(abs(list[i] - list[i + 1]) in range(1, 4) for i in range(len(list) - 1)):
+        # check if strictly increasing
+        if all(list[i] < list[i + 1] for i in range(len(list) - 1)):
+            return True
+        # check if strictly decreasing
+        if all(list[i] > list[i + 1] for i in range(len(list) - 1)):
+            return True
+    return False
+
+
+safe_reports = 0
+unsafe_levels = []
+for report in reports:
+    if check_safety(report):
+        safe_reports += 1
+    else:
+        unsafe_levels.append(report)
+
+print(unsafe_levels)
