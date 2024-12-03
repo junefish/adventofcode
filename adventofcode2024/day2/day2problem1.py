825a920601270f545461af3af3ab8f843ec0a719
junefish <junefish@users.noreply.github.com>
2024-12-03 15:11:15 -0500

    create check_safety function

--- adventofcode2024/day2/day2problem1.py
+++ adventofcode2024/day2/day2problem1.py
@@ -4,3 +4,19 @@ with open("adventofcode2024/day2/day2example.txt", "r") as input:
         reports.append([int(item) for item in line.strip().split(" ")])
 
 print(reports)
+
+
+def check_safety(list):
+    # check adjacent levels difference
+    if all(abs(list[i] - list[i + 1]) in range(1, 4) for i in range(len(list) - 1)):
+        # check if strictly increasing
+        if all(list[i] < list[i + 1] for i in range(len(list) - 1)):
+            return True
+        # check if strictly decreasing
+        if all(list[i] > list[i + 1] for i in range(len(list) - 1)):
+            return True
+    return False
+
+
+for level in reports:
+    print(check_safety(level))
