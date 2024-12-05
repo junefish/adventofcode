bd018067fca7fc77761054e4112ea20b1bb5c69e
junefish <junefish@users.noreply.github.com>
2024-12-05 01:00:10 -0500

    create scan function

--- adventofcode2024/day3/day3problem1.py
+++ adventofcode2024/day3/day3problem1.py
@@ -1,4 +1,15 @@
+import re
+
 with open("adventofcode2024/day3/day3example.txt", "r") as input:
     memory = input.readlines()[0]
 
-print(memory)
+# print(memory)
+
+
+def scan(str):
+    substring = "mul\(\d+,\d+\)"
+    result = re.findall(substring, str)
+    return result
+
+
+print(scan(memory))
