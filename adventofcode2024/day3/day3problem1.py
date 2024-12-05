dcfeb7715607f23ea096bdcc3ea7895ff1fe3341
junefish <junefish@users.noreply.github.com>
2024-12-05 01:11:11 -0500

    Revert "switch to problem input"
    Answer was too low.

--- adventofcode2024/day3/day3problem1.py
+++ adventofcode2024/day3/day3problem1.py
@@ -1,6 +1,6 @@
 import re
 
-with open("adventofcode2024/day3/day3input.txt", "r") as input:
+with open("adventofcode2024/day3/day3example.txt", "r") as input:
     memory = input.readlines()[0]
 
 
