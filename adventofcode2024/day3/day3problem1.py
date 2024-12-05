7bed7a526b3e48480b8310bc0292e292f2eba50d
junefish <junefish@users.noreply.github.com>
2024-12-05 01:06:44 -0500

    cleanup comments

--- adventofcode2024/day3/day3problem1.py
+++ adventofcode2024/day3/day3problem1.py
@@ -3,8 +3,6 @@ import re
 with open("adventofcode2024/day3/day3example.txt", "r") as input:
     memory = input.readlines()[0]
 
-# print(memory)
-
 
 def scan(str):
     substring = "mul\(\d+,\d+\)"
