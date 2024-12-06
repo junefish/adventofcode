7f9c994ac19ad26f31ea80763e4fb038e24ee8a3
junefish <junefish@users.noreply.github.com>
2024-12-05 22:19:16 -0500

    cleanup print statements

--- adventofcode2024/day3/day3problem2.py
+++ adventofcode2024/day3/day3problem2.py
@@ -3,10 +3,7 @@ import re
 with open("adventofcode2024/day3/day3example2.txt", "r") as input:
     memory = "".join(input.readlines())
 
-# print(memory)
-
 instructions = re.sub("don't\(\)", ".no.", re.sub("do\(\)", ".yes.", memory)).split(".")
-# print(instructions)
 
 
 def parse_commands(list):
