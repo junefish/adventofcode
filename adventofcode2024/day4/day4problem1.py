9529e6610f7996d686efcdf0c1bbe3991b871751
junefish <junefish@users.noreply.github.com>
2024-12-05 23:05:15 -0500

    define constants

--- adventofcode2024/day4/day4problem1.py
+++ adventofcode2024/day4/day4problem1.py
@@ -2,4 +2,9 @@ word_search = []
 with open("adventofcode2024/day4/day4example.txt", "r") as input:
     for line in input:
         word_search.append(list(line.strip()))
-print(word_search)
+# print(word_search)
+
+word = "XMAS"
+
+height = len(word_search)
+width = len(word_search[0])
