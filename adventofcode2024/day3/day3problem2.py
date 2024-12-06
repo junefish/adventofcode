d69639f0386b4aa7cd8972c449b0e4f7870f78a5
junefish <junefish@users.noreply.github.com>
2024-12-05 22:13:04 -0500

    create function to parse do/don't commands

--- adventofcode2024/day3/day3problem2.py
+++ adventofcode2024/day3/day3problem2.py
@@ -6,4 +6,26 @@ with open("adventofcode2024/day3/day3example2.txt", "r") as input:
 # print(memory)
 
 instructions = re.sub("don't\(\)", ".no.", re.sub("do\(\)", ".yes.", memory)).split(".")
-print(instructions)
+# print(instructions)
+
+
+def parse_commands(list):
+    valid = [list[0]]
+
+    i = 0
+    while i < len(list) - 1:
+        i += 1
+        if list[i] == "yes":
+            continue
+        elif list[i] == "no":
+            while list[i] != "yes":
+                i += 1
+        else:
+            valid.append(list[i])
+
+    return valid
+
+
+valid_instructions = parse_commands(instructions)
+
+print(valid_instructions)
