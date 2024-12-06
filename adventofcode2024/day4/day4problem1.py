c559ccb2c8b47cb7e5815f0a4968b520b4118c71
junefish <junefish@users.noreply.github.com>
2024-12-05 23:43:07 -0500

    create function to find all potential substrings starting with 'X'

--- adventofcode2024/day4/day4problem1.py
+++ adventofcode2024/day4/day4problem1.py
@@ -8,3 +8,26 @@ word = "XMAS"
 
 height = len(word_search)
 width = len(word_search[0])
+
+
+def find_substring(grid, i, j):
+    if grid[i][j] != "X":
+        str = ""
+    elif j > 2:  # left
+        str = grid[i][j : j - 4 : -1]
+    elif j < width - 3:  # right
+        str = grid[i][j : j + 4]
+    elif i > 2:  # up
+        str = "".join(grid[i - n][j] for n in range(4))
+    elif i < height - 3:  # down
+        str = "".join(grid[i + n][j] for n in range(4))
+    elif i > 2 and j < width - 3:  # quad-I
+        str = "".join(grid[i - n][j + n] for n in range(4))
+    elif i > 2 and j > 2:  # quad-II
+        str = "".join(grid[i - n][j - n] for n in range(4))
+    elif i < height - 3 and j > 2:  # quad-III
+        str = "".join(grid[i + n][j - n] for n in range(4))
+    elif i < height - 3 and j < width - 3:  # quad-IV
+        str = "".join(grid[i + n][j + n] for n in range(4))
+
+    return str
