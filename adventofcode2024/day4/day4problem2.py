d477557e569afbc7ca3cd358f2c7eb4920e3ff06
junefish <junefish@users.noreply.github.com>
2024-12-10 01:22:40 -0500

    switch to problem input

--- adventofcode2024/day4/day4problem2.py
+++ adventofcode2024/day4/day4problem2.py
@@ -1,5 +1,5 @@
 word_search = []
-with open("adventofcode2024/day4/day4example2.txt", "r") as input:
+with open("adventofcode2024/day4/day4input.txt", "r") as input:
     for line in input:
         word_search.append(line.strip())
 # print(word_search)
